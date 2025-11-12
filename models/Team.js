9fb457bf88885c971f7930002542ef06ba99d664
nbharathvl9 <nbharath.2255@gmail.com>
2025-11-12 23:39:25 +0530

    added google authenticaton feature.

--- models/Team.js
+++ models/Team.js
@@ -1,16 +1,30 @@
-
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const TeamSchema = new mongoose.Schema({
-  TeamName: { type: String, required: true },
-  TeamHId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  TeamMembers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }], // all members in whole team
-  HR: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  Prototype: { type: String },
-}, { timestamps: true });
+  TeamName: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  Prototype: {
+    type: String,
+  },
+  TeamHId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  TeamMembers: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  teamCode: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+});
 
-// Index to quickly find teams by head or members
-TeamSchema.index({ TeamHId: 1 });
-TeamSchema.index({ TeamMembers: 1 });
-
-module.exports = mongoose.model('Team', TeamSchema);
+module.exports = mongoose.model("Team", TeamSchema);
